ce10d8a184aa4f17e768d13db2f39ba5b44f04ed
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-01-08 01:25:34 +0100

    Implementing colorama and messages of ending connection.

--- NeuralNine/simple_chat/client.py
+++ NeuralNine/simple_chat/client.py
@@ -1,4 +1,5 @@
 import socket
+from colorama import init, Fore
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,7 +12,13 @@ while not done:
     client.send(input('Message: ').encode('utf-8'))
     msg = client.recv(1024).decode('utf-8')
 
+    init(autoreset=True)
+
     if msg == 'quit':
         done = True
     else:
-        print(msg)
+        print(Fore.MAGENTA + msg)
+
+
+client.close()
+print('Connection ended')
--- NeuralNine/simple_chat/server.py
+++ NeuralNine/simple_chat/server.py
@@ -1,4 +1,5 @@
 import socket
+from colorama import init, Fore
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,8 +12,19 @@ done = False
 
 while not done:
     msg = (client.recv(1024).decode('utf-8'))
+
+    init(autoreset=True)
+    
     if msg == 'quit':
         done = True
     else:
-        print(msg)
+        print(Fore.CYAN + msg)
+
     client.send(input('Message: ').encode('utf-8'))
+
+
+client.close()
+print('Connection ended')
+
+server.close()
+print('Server closed')
