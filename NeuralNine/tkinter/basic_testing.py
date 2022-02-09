8e87994317b6cc8b62dcc6a25f6b6536e320708c
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-02-09 23:06:08 +0100

    Two buttons with grid and place testing.

--- NeuralNine/tkinter/basic_testing.py
+++ NeuralNine/tkinter/basic_testing.py
@@ -22,8 +22,21 @@ textbox.pack(padx=BASIC_PADDING, pady=BASIC_PADDING)
 pass_entry = tk.Entry(root, font=(BASIC_FONT_TYPE, 12), width=WIN_SIZE[1], show="*", justify='center')
 pass_entry.pack(padx=BASIC_PADDING, pady=0)
 
-button = tk.Button(root, text='Pull the monke!', font=(BASIC_FONT_TYPE, 10))
-button.pack(padx=BASIC_PADDING, pady=BASIC_PADDING // 2)
+
+btn_frame = tk.Frame(root)
+btn_frame.columnconfigure(0, weight=1)
+btn_frame.columnconfigure(1, weight=1)
+btn_frame.columnconfigure(2, weight=1)
+btn_frame.columnconfigure(3, weight=1)
+
+btn_01 = tk.Button(btn_frame, text='Pull the monke!', font=(BASIC_FONT_TYPE, 10))
+btn_01.grid(row=0, column=3, sticky=tk.E)
+
+
+btn_frame.pack(padx=BASIC_PADDING, pady=BASIC_PADDING // 2, fill='x')
+
+btn_02 = tk.Button(root, text='Don\'t rush the monke!', font=(BASIC_FONT_TYPE, 10), bg='grey', fg='white')
+btn_02.place(x=WIN_SIZE[0] - 190, y=WIN_SIZE[1] - 270, height=30, width=170)
 
 
 root.mainloop()
