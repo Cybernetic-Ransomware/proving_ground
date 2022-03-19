df20732987b9f49a9cb6fd9061cf7eb3760801b1
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-03-19 15:30:53 +0100

    Get method with variables.

--- TechWithTim/FAST_API/fapi_test.py
+++ TechWithTim/FAST_API/fapi_test.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+inventory = {
+    1: {'name': 'pen',
+        'length': 'short',
+        'useful': 'high'},
+    2: {'name': 'rubber',
+        'length': 'mid',
+        'useful': 'very high'},
+    3: {'name': 'tiepin',
+        'length': 'mid',
+        'useful': 'average'},
+}
+
 
 @app.get('/')
 def home():
@@ -11,3 +23,8 @@ def home():
 @app.get('/sr')
 def second_response():
     return {'data': 'second_test'}
+
+
+@app.get('/get-item/{item_id}')
+def get_item(item_id: int):
+    return inventory[item_id]
