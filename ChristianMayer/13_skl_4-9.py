6053db0f9710d6fda4597354fff95a4fc271fdde
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-05-10 15:02:32 +0200

    Init: Example with 3 dimensions and 4 areas.

--- ChristianMayer/13_skl_4-9.py
+++ ChristianMayer/13_skl_4-9.py
@@ -5,6 +5,7 @@ from sklearn import svm
 # data: percent rate in math, language and creativity
 data = np.array([[90, 50, 60, 'TI'],
                  [100, 10, 20, 'TI'],
+                 [0, 10, 25, 'Failure'],
                  [10, 80, 10, 'Linguistics'],
                  [40, 90, 30, 'Linguistics'],
                  [5, 10, 100, 'Art'],
@@ -23,5 +24,5 @@ print(prediction)
 prediction = svm_.predict([[30, 30, 60]])
 print(prediction)
 
-prediction = svm_.predict([[80, 10, 10]])
+prediction = svm_.predict([[0, 10, 10]])
 print(prediction)
