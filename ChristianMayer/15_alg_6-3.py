0dfb513a713b6452b0e3aa3b20523acd35695043
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-05-13 00:02:28 +0200

    Init: 2 ways of calculate factorial (n!)

---
+++ ChristianMayer/15_alg_6-3.py
@@ -0,0 +1,15 @@
+import math
+
+
+n = [5, 1, 0, 4, 9, -1]
+
+
+factorial_ = lambda x: x * factorial_(x-1) if x > 1 else 1
+
+
+for number in n:
+    print(factorial_(number))
+    print(math.factorial(number))
+    print('')
+
+# math return ValueError witch negative arguments
