1fe560ae4c6893193bebd028d30df522b4128af3
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-11-28 00:08:09 +0100

    Bokeh dots graph

--- NeuralNine/bokeh_t/bokeh_data_graph_visualisations.py
+++ NeuralNine/bokeh_t/bokeh_data_graph_visualisations.py
@@ -2,16 +2,14 @@ import numpy as np
 from bokeh.plotting import figure, show
 
 
-x = np.arange(0, 10, 1)
+x = np.random.random(50) * 10
+y = np.random.random(50) * 200
 
-y1 = x ** 3 + 20
-y2 = x ** 2 + 2*x + 10
-y3 = x ** 4 - 18*x + 5
 
-p = figure(title="Example charts", x_axis_label='x', y_axis_label='y', background_fill_color='lightgrey')
+p = figure(title="Example charts", x_axis_label='x', y_axis_label='y', background_fill_color='pink')
 
-p.line(x, y1, legend_label='First function example', line_width=2, color='yellow')
-p.line(x, y2, legend_label='Second function example', line_width=2, color='brown')
-p.line(x, y3, legend_label='Third function example', line_width=2, color='orange')
+
+p.circle(x, y, legend_label='Random pouints', color='darkgreen', size=10)
+
 
 show(p)
