57884274a452656b9838e4978a072c614547c414
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-12-13 00:14:18 +0100

    Examples of parameters on calling tests

--- SixFeetUp/tests/test_search_refactored.py
+++ SixFeetUp/tests/test_search_refactored.py
@@ -6,6 +6,20 @@ from playwright.sync_api import expect, Page
 r"""
     to run with visible effects:
         py -m pytest SixFeetUp\tests --headed --slowmo 500
+        
+    to run on otcher browser engines, e.g. Firefox:
+        py -m pytest SixFeetUp\tests --headed --slowmo 500 --browser firefox       
+         
+    to run multiple on different browser engines:
+        py -m pytest SixFeetUp\tests --browser firefox --browser chromium --browser webkit -v
+        
+    to run directly on a browser (chrome vs chromium)
+        py -m pytest SixFeetUp\tests --browser-channel chrome
+        
+    to test on an emulation of a mobile device
+        py -m pytest SixFeetUp\tests --headed --slowmo 500 --device "iPad Mini"
+        list of emulable devices: 
+           https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/deviceDescriptorsSource.json  
 
 """
 
