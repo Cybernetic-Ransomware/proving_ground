537e6b1a2940f7f72fe59f151bb4c01d10eef266
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-12-26 22:00:10 +0100

    Add searching recursively, filtering by suffix and keyword, refactor to functions

--- RealPython/List of All Files by Ian Currie/directory_patches_test.py
+++ RealPython/List of All Files by Ian Currie/directory_patches_test.py
@@ -4,12 +4,44 @@ import pathlib
 files_folder = pathlib.Path("E:\LARPy - RPG\Zew Cthulhu")
 print(files_folder)
 
-#
-# for f in list(files_folder.iterdir()):
-#     print(f)
 
-print("Folders:")
-[print(f) for f in list(files_folder.iterdir()) if f.is_dir()]
-print('*' * 20)
-print('Files directly in:')
-[print(f) for f in list(files_folder.iterdir()) if f.is_file()]
+def separation() -> None:
+    print('*' * 20)
+
+
+def return_direct() -> None:
+    #
+    # for f in list(files_folder.iterdir()):
+    #     print(f)
+
+    separation()
+    print("Folders:")
+    [print(f) for f in list(files_folder.iterdir()) if f.is_dir()]
+    separation()
+    print('Files directly in:')
+    [print(f) for f in list(files_folder.iterdir()) if f.is_file()]
+
+
+# def return_all() -> None:
+#     separation()
+#     print("All folders:")
+#     [print(f) for f in list(files_folder.rglob("*")) if f.is_dir()]
+#     separation()
+#     print('All files:')
+#     [print(f) for f in list(files_folder.rglob("*")) if f.is_file()]
+
+
+def return_sufix(suffix: str='*', keyword: str='*') -> None:
+    separation()
+    print("All folders:")
+    [print(f) for f in list(files_folder.rglob(f"*{keyword}*")) if f.is_dir()]
+    separation()
+    print('All files:')
+    [print(f) for f in list(files_folder.rglob(f"*{keyword}*.{suffix}")) if f.is_file()]
+
+
+if __name__ == '__main__':
+    # return_direct()
+    # return_all()
+    # return_sufix(suffix='pdf', keyword='yellow')
+    return_sufix(keyword='1')
