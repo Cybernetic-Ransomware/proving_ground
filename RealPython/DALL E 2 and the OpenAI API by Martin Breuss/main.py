43d8b1201acc75b8ac365fbbe1e2e09d516cd706
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2023-01-18 18:06:38 +0100

    YAML parser simple reminder

--- RealPython/DALL E 2 and the OpenAI API by Martin Breuss/main.py
+++ RealPython/DALL E 2 and the OpenAI API by Martin Breuss/main.py
@@ -1,10 +1,11 @@
 import openai
 
 
-PROMPT = "A Devil over the forest and brave elf warrior protecting the viever, first person hands, stack of melee weapons, grimdark fantasy realistic painting style"
+# PROMPT = "A Devil over the forest and brave elf warrior protecting the viever, first person hands, stack of melee weapons, grimdark fantasy realistic painting style"
 # PROMPT = "An old Wizard in big round hat standing next to a vending machine in a cyberpunk japanese world, spaceport outside the window, grimdark fantasy realistic painting style"
 # PROMPT = "Deus Ex Mankind Divided syle victorian industrial hall bacground, cybernetic black shiny prosthetic hand, double helix of red filament out of printer, shoggoth"
 # PROMPT = "H. R. Giger style relief, a desperate mother at the gates of Tartar with his faithful wolf companion"
+PROMPT = "Fallout in Studio Ghibli style"
 
 
 with open('key.txt', 'r') as f:
